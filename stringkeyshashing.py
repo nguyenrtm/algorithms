73f20ed28d2d748c0628b6f919cd8c4d29621924
Nguyen Binh Nguyen <61158419+nguyenrtm@users.noreply.github.com>
2021-08-21 15:59:56 +0700

    Add files via upload

---
+++ stringkeyshashing.py
@@ -0,0 +1,42 @@
+"""Write a HashTable class that stores strings
+in a hash table, where keys are calculated
+using the first two letters of the string."""
+
+class HashTable(object):
+    def __init__(self):
+        self.table = [None]*10000
+
+    def store(self, string):
+        self.table.append(string)
+        pass
+
+    def lookup(self, string):
+        if string in self.table:
+            hash = ord(string[0])*100 + ord(string[1])
+            return hash
+        return -1
+
+    def calculate_hash_value(self, string):
+        hash = ord(string[0])*100 + ord(string[1])
+        return hash
+    
+# Setup
+hash_table = HashTable()
+
+# Test calculate_hash_value
+# Should be 8568
+print(hash_table.calculate_hash_value('UDACITY'))
+
+# Test lookup edge case
+# Should be -1
+print(hash_table.lookup('UDACITY'))
+
+# Test store
+hash_table.store('UDACITY')
+# Should be 8568
+print(hash_table.lookup('UDACITY'))
+
+# Test store edge case
+hash_table.store('UDACIOUS')
+# Should be 8568
+print(hash_table.lookup('UDACIOUS'))
